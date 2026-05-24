98aee1ab83bfb8dd07b8e96401a6960696158821
vikramrathore-vayu <vikramsinghkalwani@gmail.com>
2026-05-25 03:07:22 +0530

    Create supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://zrzbkshvrdlceasftmin.supabase.co';
+const supabaseKey = 'sb_publishable_LgliVoRGjI2rQEOSU2GuaA_Zyb36kRg';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
